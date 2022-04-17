34adb620df3f7c837b3aa18999725e56b2ed72f9
jemaji <jmateosj@gmail.com>
2022-04-17 20:43:40 +0200

    2 files modified

--- pages/bowls.js
+++ pages/bowls.js
@@ -20,25 +20,30 @@ function bowl(num) {
 }
 
 function changePum(cmp) {
-  console.log(cmp);
-  document.getElementById('pin1').className =
-    styles.bowl + ' ' + styles['pum1'];
+  const className = document.getElementById('pin1').className;
+  console.log(cmp, className);
+  document.getElementById('pin1').className += ' ' + styles['pum1'];
 }
 
 export default function Bowls() {
   return (
-    <div id="main" className={styles.main}>
-      <div></div>
-      {bowl(1)}
-      {bowl(2)}
-      {bowl(3)}
-      {bowl(4)}
-      {bowl(5)}
-      {bowl(6)}
-      {bowl(7)}
-      {bowl(8)}
-      {bowl(9)}
-      {bowl(10)}
+    <div id="container">
+      <div id="buttons">
+        <button />
+      </div>
+      <div id="score"></div>
+      <div id="bowls" className={styles.bowls}>
+        {bowl(1)}
+        {bowl(2)}
+        {bowl(3)}
+        {bowl(4)}
+        {bowl(5)}
+        {bowl(6)}
+        {bowl(7)}
+        {bowl(8)}
+        {bowl(9)}
+        {bowl(10)}
+      </div>
     </div>
   );
 }
