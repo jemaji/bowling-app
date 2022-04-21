75729c9f85f313961386f84e28d0652cb0a634a5
Jesus Mateos <jmateos@xt.mediaset.es>
2022-04-21 23:19:21 +0200

    score slider and bowls throw

--- pages/bowls.js
+++ pages/bowls.js
@@ -1,14 +1,10 @@
 import styles from '../styles/Bowls.module.css';
-import Router from 'next/router';
 import { useState } from 'react';
-import pinImage from '../assets/images/pin.png'
-import ballImage from '../assets/images/bowling-ball.png'
-
 
 export default function Bowls() {
   const getInitialRounds = () => {
-    const roundNumbers = Array.from({length: 10}, (_, i) => i+1);
-    const throwNumbers = Array.from({length: 2}, (_, i) => i+1);
+    const roundNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
+    const throwNumbers = Array.from({ length: 2 }, (_, i) => i + 1);
     return roundNumbers.reduce(
       (roundNumberAcc, roundNumberCurr) => {
         roundNumberAcc[roundNumberCurr] = throwNumbers.reduce(
@@ -37,25 +33,25 @@ export default function Bowls() {
   const reset = () => initialiceProperties();
 
   const strike = (event) => {
-    const pinNumbers = Array.from({length: 10}, (_, i) => i+1);
+    const pinNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
     rounds[currentRound][1] = pinNumbers;
-    setRounds({...rounds});
+    setRounds({ ...rounds });
     setCurrentThow(2);
   };
 
   const spare = () => {
     setCurrentThow(2);
-    const pinNumbers = Array.from({length: 10}, (_, i) => i+1);
+    const pinNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
     pinNumbers.forEach((pinNumber) => {
-      if(!rounds[currentRound][1].has(pinNumber)) {
+      if (!rounds[currentRound][1].has(pinNumber)) {
         rounds[currentRound][2].add(pinNumber);
       }
     });
-    setRounds({...rounds});
+    setRounds({ ...rounds });
   }
 
   const next = () => {
-    if(currentThow == 2 || isStrike(currentRound)) {
+    if (currentThow == 2 || isStrike(currentRound)) {
       setCurrentRound(currentRound + 1);
       setCurrentThow(1);
       return;
@@ -63,17 +59,29 @@ export default function Bowls() {
     setCurrentThow(2);
   };
 
+  const changeRollingBall = () => {
+    const classRollingBall = 'animatedBall';
+    const className = document.getElementById('ball').className;
+    if (className.includes(classRollingBall)) {
+      document.getElementById('ball').className = styles['ball'];
+    } else {
+      document.getElementById('ball').className += ` ${styles[classRollingBall]}`;
+    }
+
+  }
 
   const throwBall = () => {
     if (!asyncLock) {
       setAsyncLock(true);
       rounds[currentRound][currentThow] = new Set(animatedPins);
-      setAnimatedPins(new Set()); 
-      setRounds({...(rounds)});
+      setAnimatedPins(new Set());
+      setRounds({ ...(rounds) });
+      changeRollingBall();
       setTimeout(
         () => {
           next();
           setAsyncLock(false);
+          changeRollingBall();
         },
         1500
       );
@@ -125,23 +133,23 @@ export default function Bowls() {
     let amount = 0;
     Object.keys(rounds).forEach(
       (roundNumberAcc) => {
-        if(roundNumberAcc > roundNumber) return;
+        if (roundNumberAcc > roundNumber) return;
 
         Object.values(rounds[roundNumberAcc]).forEach((pinsNumbers) => {
           roundNumberAcc = Number(roundNumberAcc);
           amount += pinsNumbers.size;
         });
 
-        if(roundNumber == roundNumberAcc) return;
+        if (roundNumber == roundNumberAcc) return;
 
-        if(isSpare(roundNumberAcc)) {
-          amount += rounds[roundNumberAcc+1][1].size;
+        if (isSpare(roundNumberAcc)) {
+          amount += rounds[roundNumberAcc + 1][1].size;
         }
-        if(isStrike(roundNumberAcc)) {
-          if(isStrike(roundNumberAcc + 1)) {
-            amount += rounds[roundNumberAcc+1][1].size + rounds[roundNumberAcc+2][1].size;
+        if (isStrike(roundNumberAcc)) {
+          if (isStrike(roundNumberAcc + 1)) {
+            amount += rounds[roundNumberAcc + 1][1].size + rounds[roundNumberAcc + 2][1].size;
           } else {
-            amount += rounds[roundNumberAcc+1][1].size + rounds[roundNumberAcc+1][2].size;
+            amount += rounds[roundNumberAcc + 1][1].size + rounds[roundNumberAcc + 1][2].size;
           }
         }
       }
@@ -159,15 +167,17 @@ export default function Bowls() {
     return round[1].size == 10;
   };
 
-  const score = () => (
-    <div id="scoreBox" className={styles.scoreBox}>
-      <div id="headerBox" className={styles.headerBox}>{currentRound}</div>
-      <div id="bodyBox" className={styles.bodyBox}>
-        <div id="rowA" className={styles.rowA}>
-          <div id="columnA" className={styles.columnA}>{showScoreFirstThrow()}</div>
-          <div id="columnB" className={styles.columnB}>{showScoreSecondThrow()}</div>
+  const score = (num) => (
+    <div id={'slide-' + num} className={styles['slides-item']}>
+      <div id={'scoreBox' + num} className={styles.scoreBox}>
+        <div id={'headerBox' + num} className={styles.headerBox}>{num}</div>
+        <div id={'bodyBox' + num} className={styles.bodyBox}>
+          <div id={'rowA' + num} className={styles.rowA}>
+            <div id={'columnA' + num} className={styles.columnA}>{showScoreFirstThrow()}</div>
+            <div id={'columnB' + num} className={styles.columnB}>{showScoreSecondThrow()}</div>
+          </div>
+          <div id={'rowB' + num} className={styles.rowB}>{getScore(num)}</div>
         </div>
-        <div id="rowB" className={styles.rowB}>{getScore(currentRound)}</div>
       </div>
     </div>
   );
@@ -187,7 +197,6 @@ export default function Bowls() {
           (animatedPins.has(num) ? ' ' + styles.animatedPin : '')
         }
         onClick={() => changePinActivation(num)}
-        style={{backgroundImage: `url(${pinImage.src})`}}
       ></div>
       <div className={
         styles.pinShawod +
@@ -204,7 +213,32 @@ export default function Bowls() {
         <button id="spare" onClick={spare}>SPARE</button>
         <button id="next" onClick={next}>NEXT</button>
       </div>
-      {score()}
+      <div className={styles.carousel}>
+        <div className={styles.slides}>
+          {score(1)}
+          {score(2)}
+          {score(3)}
+          {score(4)}
+          {score(5)}
+          {score(6)}
+          {score(7)}
+          {score(8)}
+          {score(9)}
+          {score(10)}
+        </div>
+        <div className={styles.carousel__nav}>
+          <a className={styles['slider-nav']} href="#slide-1">1</a>
+          <a className={styles['slider-nav']} href="#slide-2">2</a>
+          <a className={styles['slider-nav']} href="#slide-3">3</a>
+          <a className={styles['slider-nav']} href="#slide-4">4</a>
+          <a className={styles['slider-nav']} href="#slide-5">5</a>
+          <a className={styles['slider-nav']} href="#slide-6">6</a>
+          <a className={styles['slider-nav']} href="#slide-7">7</a>
+          <a className={styles['slider-nav']} href="#slide-8">8</a>
+          <a className={styles['slider-nav']} href="#slide-9">9</a>
+          <a className={styles['slider-nav']} href="#slide-10">10</a>
+        </div>
+      </div>
       <div id="bowls" className={styles.bowls}>
         {bowl(1)}
         {bowl(2)}
@@ -217,8 +251,7 @@ export default function Bowls() {
         {bowl(9)}
         {bowl(10)}
       </div>
-      <div id="ball" className={styles.ball} style={{backgroundImage: `url(${ballImage.src})`}} onClick={throwBall}>
-        <h2 style={{textAlign: `center`}}>LANZAR</h2>
+      <div id="ball" className={styles.ball} onClick={throwBall}>
       </div>
     </div>
   );
